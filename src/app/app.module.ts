2375152cbe7c2e2f98fac285fa7ee1f05b91bee3
Abinwc <abinpaul5598@gmail.com>
2024-09-24 13:07:04 +0530

    save

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,19 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { FooterComponent } from './footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { FormsModule } from '@angular/forms';
-import { ScrollColorChange } from './common-utils/background-color-change-on-scroll.directive';
-import { ScrollChangeColorDirective } from './scroll-change-color.directive';
-import { AnimationStateService } from './common-utils/animation-state.service';
-import { InternshipPosterComponent } from './internship-poster/internship-poster.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { NavbarComponent } from "./navbar/navbar.component";
+import { FooterComponent } from "./footer/footer.component";
+import { HomeComponent } from "./home/home.component";
+import { FormsModule } from "@angular/forms";
+import { ScrollColorChange } from "./common-utils/background-color-change-on-scroll.directive";
+import { ScrollChangeColorDirective } from "./scroll-change-color.directive";
+import { AnimationStateService } from "./common-utils/animation-state.service";
+import { InternshipPosterComponent } from "./internship-poster/internship-poster.component";
+import { provideFirestore, getFirestore } from "@angular/fire/firestore";
+import { initializeApp, provideFirebaseApp } from "@angular/fire/app";
+import { environment } from "../environments/environment";
 
 @NgModule({
   declarations: [
@@ -23,7 +26,11 @@ import { InternshipPosterComponent } from './internship-poster/internship-poster
     InternshipPosterComponent,
   ],
   imports: [BrowserModule, AppRoutingModule, FormsModule],
-  providers: [AnimationStateService],
+  providers: [
+    AnimationStateService,
+    provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
+    provideFirestore(() => getFirestore()),
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ src/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+  firebaseConfig: {
+    apiKey: "AIzaSyCg9_46nzsuXoBuUt_NI8I1IwfVtGUPqAc",
+    authDomain: "techcora-site-1.firebaseapp.com",
+    projectId: "techcora-site-1",
+    storageBucket: "techcora-site-1.appspot.com",
+    messagingSenderId: "814583102581",
+    appId: "1:814583102581:web:58e660f0de68278c688c3c",
+  },
+};
